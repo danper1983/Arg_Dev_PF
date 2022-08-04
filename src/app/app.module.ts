024e68117cc747c72a143120df4dc752ed3adf5d
danper1983 <danper_ar@hotmail.com>
2022-08-04 18:34:25 -0300

    generacion de modulos y otros detalles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,25 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
 import { LogoARGPComponent } from './component/header/logo-argp/logo-argp.component';
 import { RsocialesComponent } from './component/header/rsociales/rsociales.component';
+import { AcercadeComponent } from './component/acercade/acercade.component';
+import { ExperienciaComponent } from './component/experiencia/experiencia.component';
+import { EducacionComponent } from './component/educacion/educacion.component';
+import { SkillsComponent } from './component/skills/skills.component';
+import { ProyectosComponent } from './component/proyectos/proyectos.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogoARGPComponent,
-    RsocialesComponent
+    RsocialesComponent,
+    AcercadeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    SkillsComponent,
+    ProyectosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
