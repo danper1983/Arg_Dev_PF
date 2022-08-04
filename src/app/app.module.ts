360af3a1c7656cbbebdb17e7782c7ec6049e3453
danper1983 <danper_ar@hotmail.com>
2022-08-04 17:54:44 -0300

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { LogoARGPComponent } from './component/header/logo-argp/logo-argp.component';
+import { RsocialesComponent } from './component/header/rsociales/rsociales.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoARGPComponent,
+    RsocialesComponent
   ],
   imports: [
     BrowserModule,
